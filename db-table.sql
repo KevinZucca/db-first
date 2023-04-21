692762df78b5e38e092c4096ce691f731a3a3226
KevinZucca <kev.94@live.it>
2023-04-21 16:31:28 +0200

    feat: first db table

---
+++ db-table.sql
@@ -0,0 +1,26 @@
+
+//FIRST DB - CARS //
+
+
+
+ID - SMALLINT - Primary key - NOT_NULL - AUTO_INCREMENT - UNIQUE
+model - VARCHAR(100) - NOT_NULL
+license_plate - VARCHAR(10) - NOT_NULL - UNIQUE
+production_house - VARCHAR(100) - NOT_NULL
+production_year - SMALLINT - NOT_NULL
+original_price - INT - NULL
+discount - INT - NULL
+final_price - INT - NOT_NULL 
+fuel_type - VARCHAR(20) - NOT_NULL
+doors_number - VARCHAR(10) - NULL
+engine_s_power_number - INT - NULL
+transmission_system - VARCHAR(20) - NULL
+kilometers - INT - NOT_NULL
+ 
+
+
+
+
+
+
+
